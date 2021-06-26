58590ec13e35b5fccd402f3559caea6703bef780
paridhi1314 <61877547+paridhi1314@users.noreply.github.com>
2021-06-26 16:48:22 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Javascript and REact.js</h1>
+      <p>A basic web dev React js Bootcamp</p>
+
+      
+    </div>
+  );
+}
+export default Info;
